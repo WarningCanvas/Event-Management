fe2169a0ab47d2092630e8d3709114abf4a5b7c0
WarningCanvas <dhimankrishiva3@gmail.com>
2024-10-21 02:00:59 +0530

    Add files via upload

--- website/homepage.js
+++ website/homepage.js
@@ -1,1 +1,27 @@
+// Function to check if element is in the viewport
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Function to add the 'show' class to elements in view
+function checkVisibility() {
+    const textBox = document.querySelector('.text-box');
+    const imageBox = document.querySelector('.image-box');
+    
+    if (isElementInViewport(textBox)) {
+        textBox.classList.add('show');
+    }
+    if (isElementInViewport(imageBox)) {
+        imageBox.classList.add('show');
+    }
+}
+
+// Event listener for scroll event
+window.addEventListener('scroll', checkVisibility);
 
